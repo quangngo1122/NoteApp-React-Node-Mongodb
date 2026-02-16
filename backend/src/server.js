e6d00881576ac60c4b1276ba44215f05cd46340e
quangngo1122 <mrquang9a1apt@gmail.com>
2026-02-16 11:24:52 +0700

    feat(deploy): chỉnh lại một số logic chuẩn bị cho việc deploy lên render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,35 @@
-import express from 'express';
+import express from "express";
 
-import tasksRoute from './routes/tasksRoutes.js'
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import cors from "cors"
+import tasksRoute from "./routes/tasksRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path, { dirname } from "path";
 dotenv.config();
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 
 const app = express();
 
+app.use(express.json()); // middleware level 1 chuyen du lieu tu json thanh obj cho de xu ly
 
-app.use(express.json());// middleware level 1 chuyen du lieu tu json thanh obj cho de xu ly
+if (process.env.NOTE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
-app.use(cors({origin:"http://localhost:5173"}));
+app.use("/api/tasks", tasksRoute);
+if (process.env.NOTE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.use("/api/tasks",tasksRoute);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Cổng bắt đầu trên cổng ${PORT}`)
-    })
-})
-
-
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Cổng bắt đầu trên cổng ${PORT}`);
+  });
+});
